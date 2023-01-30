de6e5d784b7e63d0ce1a7d38b91b70f9cc17c05d
leirdan <uouleirdan@gmail.com>
2023-01-29 21:33:53 -0300

    refactor: requestPokemon function and add some comments

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ form.appendChild(button);
 
 div.appendChild(form);
 
+const divResult = document.getElementById("result");
+const divError = document.getElementById("error");
+
+const message = document.createElement("h4");
+let text;
+
 /* COMMUNICATION WITH POKEAPI */
 
 const endpoint = `https://pokeapi.co/api/v2/pokemon/`;
@@ -29,27 +35,53 @@ const getElementForm = (element) => {
 	return document.querySelector(element);
 };
 
+// here, we get the elements from html.
 const searchInput = getElementForm(".search-input");
 const searchButton = getElementForm(".search-button");
 
 let pokemonName, pokemonResult;
 
-searchButton.addEventListener("click", (event) => {
-	event.preventDefault();
-	pokemonName = searchInput.value.toLowerCase();
-	requestPokemon(endpoint, pokemonName);
-});
+/******** FUNCTIONS *********/
 
-const requestPokemon = (endpoint, pokemon) => {
-	fetch(`${endpoint}${pokemon}`)
-		.then((result) => {
-			return result.json();
-		})
-		.then((data) => {
-			pokemonResult = data;
-			console.log(pokemonResult);
+// 1. the function below is the one that make the request in pokeApi and return an error or the pokemon data!
+async function requestPokemon(endpoint, pokemon) {
+	const requestResult = await fetch(`${endpoint}${pokemon}`);
+	if (requestResult.status === 200) {
+		const data = await requestResult.json();
+		pokemonResult = data;
+		return pokemonResult;
+	} else {
+		text = document.createTextNode(`insert a pokemon that exists!`);
+		message.appendChild(text);
+		divError.appendChild(message);
+	}
+}
+
+// 2. the function below create the html which contains the pokemon's info that will be rendered.
+async function createCard(pokemon) {
+	return `
+    <div> 
+    <h1 class="name"> Name: ${pokemon.name} </h1>
+    </div>
+    `;
+}
+
+// 3. this is the function that calls the request API function and print the pokemon's data in the screen
+async function startApp(pokemon) {
+	await requestPokemon(endpoint, pokemon)
+		.then((pokemon) => {
+			divResult.innerHTML = createCard(pokemon);
 		})
 		.catch((err) => {
-			console.log(err);
+			text = document.createTextNode(err);
+			message.appendChild(text);
+			divError.appendChild(message);
 		});
-};
+}
+
+// 4. the event that is activated by a click in the submit button. gets the value in search input and execute the startApp function with the value of 	parameter.
+searchButton.addEventListener("click", async (event) => {
+	event.preventDefault();
+	pokemonName = searchInput.value.toLowerCase();
+	startApp(pokemonName);
+});
